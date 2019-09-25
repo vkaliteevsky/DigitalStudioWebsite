58978a6e589096eb294620dcb26365f911bf9e37
Vasiliy Kaliteevskiy <vkalit@gmail.com>
2019-09-25 12:38:51 +0300

    Project upload
    Digital Studio Website upload

---
+++ Digital Studio Website/dig-studio.ru/php/mailer.php
@@ -0,0 +1,23 @@
+﻿<?php
+	$inputEmail = $_POST['inputEmail'];
+	$inputPhone = $_POST['inputPhone'];
+	$inputName = $_POST['inputName'];
+	$inputComment = $_POST['inputComment'];
+	
+	$textBody = "New Order!\nEmail: " . $inputEmail . "\n" . "Phone: " . $inputPhone . "\n" . "Name:" . 
+				$inputName . "\n" . "Comment: " . $inputComment . "\n";
+	
+	echo $textBody;
+
+	$to      = 'gik06@yandex.ru';
+	$subject = 'New Order for Digital Studio';
+	$message = $textBody;
+	$headers = 'From: dig-studio' . "\r\n" .
+		'Reply-To: dig-studio.ru' . "\r\n" .
+		'Content-type: text/html; charset=\"utf-8\"' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	mail($to, $subject, $message, $headers);
+	
+	$result = array("status" => "200");
+    echo json_encode($result);
+?>
